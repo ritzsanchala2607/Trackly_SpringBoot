55598a3bd3794955435e78711a60a826dbf662df
ritzsanchala2607 <ritzsanchala2607@gmail.com>
2025-05-15 08:31:43 +0530

    Developed APIS TO Add and Fetch Users Credentials

---
+++ backend/src/main/java/com/LMS/Trackly/Entity/Cred.java
@@ -0,0 +1,94 @@
+package com.LMS.Trackly.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Cred {
+
+    public Cred(){
+
+    }
+
+    public Cred(String email, String username, String password, String role, String district, String phoneNumber) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.district = district;
+        this.phoneNumber = phoneNumber;
+    }
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "email")
+    private String email;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "role")
+    private String role;
+    @Column(name = "district")
+    private String district;
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ backend/src/main/java/com/LMS/Trackly/Repository/CredRepository.java
@@ -0,0 +1,7 @@
+package com.LMS.Trackly.Repository;
+
+import com.LMS.Trackly.Entity.Cred;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CredRepository extends JpaRepository<Cred,Object> {
+}
