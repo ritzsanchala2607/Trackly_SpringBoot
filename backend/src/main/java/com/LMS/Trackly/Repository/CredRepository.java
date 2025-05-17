d3ec1f8095323dbe3b0782c1b425daf6d57c8d2d
ritzsanchala2607 <ritzsanchala2607@gmail.com>
2025-05-17 16:02:37 +0530

    Developed Login Api and allow cross origin

--- backend/src/main/java/com/LMS/Trackly/Repository/CredRepository.java
+++ backend/src/main/java/com/LMS/Trackly/Repository/CredRepository.java
@@ -3,5 +3,8 @@ package com.LMS.Trackly.Repository;
 import com.LMS.Trackly.Entity.Cred;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CredRepository extends JpaRepository<Cred,Object> {
+    Optional<Cred> findByEmail(String email);
 }
