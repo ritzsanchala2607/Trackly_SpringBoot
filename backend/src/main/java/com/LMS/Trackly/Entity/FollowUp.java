285b4a91970e7c35cad2efbb30d7328f7164207d
Aryanlanghanoja <aryanlanghanoja233@gmail.com>
2025-05-17 14:17:46 +0530

    added db config

--- backend/src/main/java/com/LMS/Trackly/Entity/FollowUp.java
+++ backend/src/main/java/com/LMS/Trackly/Entity/FollowUp.java
@@ -1,10 +1,25 @@
 package com.LMS.Trackly.Entity;
 
-import jakarta.persistence.*;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "follow_up")
+@JsonIgnoreProperties({
+    "hibernateLazyInitializer", "handler"
+})
 public class FollowUp {
 
     @Id
